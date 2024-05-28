46f7351b36b36f47e6203b6fd809e3029bcabc0a
alokMIPL <alok@madhulata.com>
2024-05-28 14:04:30 +0530

    routing-marketing

---
+++ src/routes/(marketing)/about/+page.svelte
@@ -0,0 +1,17 @@
+
+<script lang="ts">
+
+</script>
+
+<svelte:head>
+  <title>About</title>
+</svelte:head>
+
+<h1>Welcome to About Page </h1>
+<p>This is about page </p>
+
+
+
+<style>
+  
+</style>
